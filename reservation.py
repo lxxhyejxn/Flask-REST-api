c3af50f56849fcd8c11ed6262d06b81743fbf2f6
hyejin lee <107865303+lxxhyejxn@users.noreply.github.com>
2024-04-17 14:23:47 +0900

    Update reservation.py

--- reservation.py
+++ reservation.py
@@ -135,8 +135,6 @@ def confirm():
 
     return jsonify(result), 200
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host='0.0.0.0' , port=5000)
-
 if __name__ == "__main__":
-    app.run(debug=True, host='10.0.0.4' , port=5000)
+    app.run(debug=True, host='0.0.0.0' , port=5000)
+
